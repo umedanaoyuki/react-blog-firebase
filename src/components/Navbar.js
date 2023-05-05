9f0a7c01019b521a0a8be417c3f0e516d74daadd
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2023-05-05 19:42:06 +0900

    微調整

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,19 +2,24 @@ import React from 'react'
 import { Link } from "react-router-dom";
 import "./Navbar.css";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
-
+import { faHouse, faPen, faRightToBracket } from '@fortawesome/free-solid-svg-icons'
 
 
 const Navbar = () => {
   return (
     <nav>
         <Link to="/">
-            <FontAwesomeIcon icon={faEnvelope}></FontAwesomeIcon>
+            <FontAwesomeIcon icon={faHouse}></FontAwesomeIcon>
             ホーム
             </Link>
-        <Link to="/createpost">記事投稿</Link>
-        <Link to="/login">ログイン</Link>
+        <Link to="/createpost">
+            <FontAwesomeIcon icon={faPen}></FontAwesomeIcon>
+            記事投稿
+            </Link>
+        <Link to="/login">
+            <FontAwesomeIcon icon={faRightToBracket}></FontAwesomeIcon>
+            ログイン
+            </Link>
     </nav>
   )
 };
