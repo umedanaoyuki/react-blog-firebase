c19ad0df29b04c828d5b8f491603aabdd5eaa061
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2023-05-05 19:32:04 +0900

    fontaswesome追加

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import { Link } from "react-router-dom";
 import "./Navbar.css";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+
 
 
 const Navbar = () => {
   return (
     <nav>
-        <Link to="/">ホーム</Link>
+        <Link to="/">
+            <FontAwesomeIcon icon={faEnvelope}></FontAwesomeIcon>
+            ホーム
+            </Link>
         <Link to="/createpost">記事投稿</Link>
         <Link to="/login">ログイン</Link>
     </nav>
