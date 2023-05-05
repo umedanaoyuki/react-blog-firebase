ae421a1d0cc4b17d10573bc471735d60082fb980
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2023-05-05 19:48:15 +0900

    ログイン機能実装

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,9 +1,26 @@
+import { signInWithPopup } from 'firebase/auth'
 import React from 'react'
+import { auth, provider } from '../firebase'
+
+const Login = () => {
+  const loginWithGoogle = () => {
+
+    signInWithPopup(auth, provider).then((result) => {
+      
+
+
+    }).catch
+
+  }
+}
 
 const Login = () => {
   return (
-    <div>Login</div>
-  )
+    <div>
+      <p>ログインして始める</p>
+      <button onClick={loginWithGoogle}>Googleでログイン</button>
+    </div>
+  );
 }
 
 export default Login
