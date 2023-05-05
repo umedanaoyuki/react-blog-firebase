7083610d4115bb7af0d24decc3a93bcc508db99b
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2023-05-05 20:41:17 +0900

    ログアウト処理実装

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -1,9 +1,28 @@
+import { getAuth, signOut } from 'firebase/auth'
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
-const Logout = () => {
+const Logout = ({setIsAuth}) => {
+
+  const navigate = useNavigate();
+  const auth = getAuth();
+
+  const logout = () => {
+    //ログアウト
+    signOut(auth).then(() => {
+      console.log("ログアウト実行")
+      localStorage.clear();
+      //setIsAuth(false);
+      navigate("/login");
+    });
+  }
+
   return (
-    <div>Logout</div>
-  )
+    <div>
+      <p>ログアウトする</p>
+      <button onClick={logout}>ログアウト</button>
+    </div>
+  );
 }
 
 export default Logout
