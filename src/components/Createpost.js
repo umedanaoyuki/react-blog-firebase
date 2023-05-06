ac82438b6b0b2e065785896693206cc96efc3b35
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2023-05-06 12:15:33 +0900

    記事投稿のHTML作成

--- src/components/Createpost.js
+++ src/components/Createpost.js
@@ -2,8 +2,21 @@ import React from 'react'
 
 const Createpost = () => {
   return (
-    <div>Createpost</div>
+    <div className="createPostPage">
+      <div className="postContainer">
+        <h1>記事を投稿する</h1>
+        <div className='inputPost'>
+          <div>タイトル</div>
+          <input type="text" placeholder="タイトルを記入"></input>
+        </div>
+        <div className='inputPost'>
+          <div>投稿</div>
+          <textarea placeholder="投稿内容を記入"></textarea>
+        </div>
+        <button className='postButton'>投稿する</button>
+      </div>
+    </div>
   )
-}
+};
 
 export default Createpost
