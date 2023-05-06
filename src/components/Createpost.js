ee2447ff3828084943fe97245bead67362175dda
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2023-05-06 12:43:42 +0900

    記事投稿内容を状態に保存（useState）

--- src/components/Createpost.js
+++ src/components/Createpost.js
@@ -1,21 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './CreatePost.css';
 
 
 const Createpost = () => {
+  const [title, setTitle] = useState();
+  const [postText, setPostText] = useState();
+
+  const createPost = () => {
+    console.log(title);
+    console.log(postText);
+  }
+
+
   return (
     <div className="createPostPage">
       <div className="postContainer">
         <h1>記事を投稿する</h1>
         <div className='inputPost'>
           <div>タイトル</div>
-          <input type="text" placeholder="タイトルを記入"></input>
+          <input type="text" placeholder="タイトルを記入" onChange={(e) => setTitle(e.target.value)}/>
         </div>
         <div className='inputPost'>
           <div>投稿</div>
-          <textarea placeholder="投稿内容を記入"></textarea>
+          <textarea placeholder="投稿内容を記入" onChange={(e) => setPostText(e.target.value)}/>
         </div>
-        <button className='postButton'>投稿する</button>
+        <button className='postButton' onClick={createPost}>投稿する</button>
       </div>
     </div>
   )
