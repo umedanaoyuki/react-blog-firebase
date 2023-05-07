06f27e1fe907810621433487e1caca9796e9a7db
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2023-05-07 17:41:24 +0900

    firebaseから情報をとってくる

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,23 @@
-import React from 'react';
+import { async } from '@firebase/util';
+import { collection, doc, DocumentSnapshot, getDocs } from 'firebase/firestore';
+import React, { useEffect } from 'react';
+import { db } from '../firebase';
 import "./Home.css";
 
 function Home() {
+
+  useEffect(() => {
+    const getPosts = async() => {
+        const data = await getDocs(collection(db, "posts"));
+        console.log(data);
+
+        console.log(data.log);
+        console.log(data.docs.map((doc) => ({doc})));
+        console.log(data.docs.map((doc) => ({...doc.data(), id: doc.id})));
+    };
+    getPosts();
+  }, []);
+
   return (
     <div className="homePage">
       <div className="postContents">
