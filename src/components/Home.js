e768e1754c0c98de79d27d686b6dc8e64470877d
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2023-05-07 18:13:01 +0900

    ゲットしたデータを画面へ出力

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,38 +1,42 @@
 import { async } from '@firebase/util';
 import { collection, doc, DocumentSnapshot, getDocs } from 'firebase/firestore';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { db } from '../firebase';
 import "./Home.css";
 
 function Home() {
-
+  const [postList, setPostList] = useState([]);
+  
   useEffect(() => {
     const getPosts = async() => {
         const data = await getDocs(collection(db, "posts"));
-        console.log(data);
-
-        console.log(data.log);
-        console.log(data.docs.map((doc) => ({doc})));
-        //ここの取り出し方は難しい
-        console.log(data.docs.map((doc) => ({...doc.data(), id: doc.id})));
+        // console.log(data);
+        // console.log(data.log);
+        // console.log(data.docs.map((doc) => ({doc})));
+        // //ここの取り出し方は難しい
+        // console.log(data.docs.map((doc) => ({...doc.data(), id: doc.id})));
+        setPostList(data.docs.map((doc) => ({...doc.data(), id: doc.id}))); 
     };
     getPosts();
+    console.log(postList);
   }, []);
 
   return (
     <div className="homePage">
-      <div className="postContents">
-        <div className="postHeader">
-          <h1>タイトル</h1>
+      {postList.map((post) => {
+        return (
+          <div className="postContents">
+          <div className="postHeader">
+            <h1>{post.title}</h1>
+          </div>
+          <div className='postTextContents'>{post.postText}</div>
+          <div className='nameAndDeletButton'>
+            <h3>@{post.author.userName}</h3>
+            <button>削除</button>
+          </div>
         </div>
-        <div className='postTextContents'>
-          今はReactの学習中です。今はReactの学習中です。今はReactの学習中です。今はReactの学習中です。今はReactの学習中です。今はReactの学習中です。今はReactの学習中です。今はReactの学習中です。今はReactの学習中です。今はReactの学習中です。今はReactの学習中です。今はReactの学習中です。今はReactの学習中です。今はReactの学習中です。今はReactの学習中です。今はReactの学習中です。今はReactの学習中です。今はReactの学習中です。
-        </div>
-        <div className='nameAndDeletButton'>
-          <h3>@Umeda</h3>
-          <button>削除</button>
-        </div>
-      </div>
+        );
+      })}
     </div>
   )
 }
